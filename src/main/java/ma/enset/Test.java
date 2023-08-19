31706cbcac48cb77e0fd0c04482985cd5b83cbde
unknown <sidimedmedmoctar691@gmail.com>
2023-08-19 15:26:50 +0100

    comments

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -1,6 +1,10 @@
+/*
+ * comments
+ */
 package ma.enset;
 public class Test{
     public static void main(String[] args){
+        //coment-----------------
         System.out.println("Hello !");
     }
 }
